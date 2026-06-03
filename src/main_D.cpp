8357febcf53246e16ccc3975746eec1bf96ceda9
BrandonT116 <btendilla16@gmail.com>
2026-06-03 14:20:25 -0500

    updated main_D to have proper pin mappings for row and col multiplexers

--- src/main_D.cpp
+++ src/main_D.cpp
@@ -15,61 +15,144 @@
     MUX1_0-7 = FDC CH0 readings for MUX1 states 0–7
 */
 
-// MUX pin mapping 
-// #define MUX2_C 26 // MSB
-// #define MUX2_B 27 //    
-// #define MUX2_A 14 // LSB
- 
-// TEST 2x2
-#define MUX2_A1 27
-#define MUX2_A0 25
+// TEST 4x4
+#define MUX_COL_S2 25
+#define MUX_COL_S1 33
+#define MUX_COL_S0 32
 
-#define MUX1_A1 14
-#define MUX1_A0 32
-
-// #define MUX1_C 25 // MSB
-// #define MUX1_B 33 //  
-// #define MUX1_A 32 // LSB
+#define MUX_ROW_S2 26
+#define MUX_ROW_S1 27
+#define MUX_ROW_S0 14
 
 // Constants
-#define MUX_STATES        2  // 8 states for 3 select lines (8:1 mux)
+#define MUX_STATES        4 // 8 states for 3 select lines (8:1 mux)
 #define FDC_CHANNELS       1   // Using 1 FDC channel (CH0)
-#define TOTAL_READINGS     4
-#define SCAN_PRINT 4
+#define TOTAL_READINGS     16
+#define SCAN_PRINT 16
 
 FDC2214 fdc1(FDC2214_I2C_ADDR_1);
-
-void setMuxPins(int s1, int s0, int state) {
-  digitalWrite(s1, (state >> 1) & 0x01); 
-  digitalWrite(s0, (state >> 0) & 0x01); 
-  // digitalWrite(s2, (state >> 0) & 0x01);
-}
 
 void initFDC(FDC2214 &fdc, const char *name) {
   // Enable only CH0 and CH1 for faster conversion
   // 0x3 = binary 0011 = CH0 and CH1 enabled
   // 0x4 = autoscan sequence CH0->CH1
-
-  bool ok = fdc.begin(0x01, 0x04, 0x05, true); // chanMask=0x01 (CH0), autoscanSeq=0 (no autoscan), deglitchValue=0 (default), intOsc=true
-
+  // chanMask=0x01 (CH0), autoscanSeq=0 (no autoscan), deglitchValue=0 (default), intOsc=true
+  bool ok = fdc.begin(0x01, 0x04, 0x05, true); 
   // if (ok) Serial.print(name), Serial.println(" OK");
   // else Serial.print(name), Serial.println(" FAIL");
 }
 
-// void setupMuxPins() {
-//   pinMode(MUX1_C, OUTPUT);
-//   pinMode(MUX1_B, OUTPUT);
-//   pinMode(MUX1_A, OUTPUT);
-//   pinMode(MUX2_C, OUTPUT);
-//   pinMode(MUX2_B, OUTPUT);
-//   pinMode(MUX2_A, OUTPUT);
-// }
+void set_row(int s2, int s1, int s0, int state) {
+  // 
+  switch(state) {
+    case 0:
+      digitalWrite(s2, HIGH);
+      digitalWrite(s1, HIGH);
+      digitalWrite(s0, HIGH);
+      break;
+    case 1:
+      digitalWrite(s2, HIGH);
+      digitalWrite(s1, LOW);
+      digitalWrite(s0, HIGH);
+      break;
+    case 2:
+      digitalWrite(s2, HIGH);
+      digitalWrite(s1, HIGH);
+      digitalWrite(s0, LOW);
+      break;
+    case 3:
+      digitalWrite(s2, HIGH);
+      digitalWrite(s1, LOW);
+      digitalWrite(s0, LOW);
+      break;
+    case 4:
+      digitalWrite(s2, LOW);
+      digitalWrite(s1, HIGH);
+      digitalWrite(s0, LOW);
+      break;
+    case 5:
+      digitalWrite(s2, LOW);
+      digitalWrite(s1, LOW);
+      digitalWrite(s0, HIGH);
+      break;
+    case 6:
+      digitalWrite(s2, LOW);
+      digitalWrite(s1, LOW);
+      digitalWrite(s0, LOW);
+      break;
+    case 7:
+      digitalWrite(s2, LOW);
+      digitalWrite(s1, HIGH);
+      digitalWrite(s0, HIGH);
+      break;
+    default:
+      // Invalid state, default to 0
+      digitalWrite(s2, LOW);
+      digitalWrite(s1, LOW);
+      digitalWrite(s0, LOW);
+      break;
+  }
+}
+
+void set_col(int s2, int s1, int s0, int state) {
+  // 
+  switch(state) {
+    case 0:
+      digitalWrite(s2, LOW);
+      digitalWrite(s1, HIGH);
+      digitalWrite(s0, HIGH);
+      break;
+    case 1:
+      digitalWrite(s2, LOW);
+      digitalWrite(s1, LOW);
+      digitalWrite(s0, LOW);
+      break;
+    case 2:
+      digitalWrite(s2, LOW);
+      digitalWrite(s1, LOW);
+      digitalWrite(s0, HIGH);
+      break;
+    case 3:
+      digitalWrite(s2, LOW);
+      digitalWrite(s1, HIGH);
+      digitalWrite(s0, LOW);
+      break;
+    case 4:
+      digitalWrite(s2, HIGH);
+      digitalWrite(s1, LOW);
+      digitalWrite(s0, LOW);
+      break;
+    case 5:
+      digitalWrite(s2, HIGH);
+      digitalWrite(s1, HIGH);
+      digitalWrite(s0, LOW);
+      break;
+    case 6:
+      digitalWrite(s2, HIGH);
+      digitalWrite(s1, LOW);
+      digitalWrite(s0, HIGH);
+      break;
+    case 7:
+      digitalWrite(s2, HIGH);
+      digitalWrite(s1, HIGH);
+      digitalWrite(s0, HIGH);
+      break;
+    default:
+      // Invalid state, default to 0
+      digitalWrite(s2, LOW);
+      digitalWrite(s1, LOW);
+      digitalWrite(s0, LOW);
+      break;
+  }
+}
 
 void setupMuxPins() {
-  pinMode(MUX1_A1, OUTPUT);
-  pinMode(MUX1_A0, OUTPUT);
-  pinMode(MUX2_A1, OUTPUT);
-  pinMode(MUX2_A0, OUTPUT);
+  pinMode(MUX_COL_S2, OUTPUT);
+  pinMode(MUX_COL_S1, OUTPUT);
+  pinMode(MUX_COL_S0, OUTPUT);
+  pinMode(MUX_ROW_S2, OUTPUT);
+  pinMode(MUX_ROW_S1, OUTPUT);
+  pinMode(MUX_ROW_S0, OUTPUT);
 }
 
 void setup() {
@@ -78,21 +161,11 @@ void setup() {
   Serial.begin(250000);
 
   setupMuxPins();
-  
-  // Initialize both muxes to state 0
-  // setMuxPins(MUX1_C, MUX1_B, MUX1_A, 0);
-  // setMuxPins(MUX2_C, MUX2_B, MUX2_A, 0);
-  
-  setMuxPins(MUX1_A1, MUX1_A0, 0);
-  setMuxPins(MUX2_A1, MUX2_A0, 0);
+  set_col(MUX_COL_S2, MUX_COL_S1, MUX_COL_S0, 0);
+  set_row(MUX_ROW_S2, MUX_ROW_S1, MUX_ROW_S0, 0);
 
   initFDC(fdc1, "FDC");
-
   fdc1.enterSleepMode(); 
-  
-  // Serial.println("Starting multiplexed capacitance scan (RAW)...");
-  
-  // Let FDC stabilize with initial mux state
   delay(100);
 }
 
@@ -104,11 +177,10 @@ void loop() {
   int idx = 0;
   
   for (int mux1 = 0; mux1 < MUX_STATES; mux1++) {
-   setMuxPins(MUX1_A1, MUX1_A0, mux1);
+   set_row(MUX_ROW_S2, MUX_ROW_S1, MUX_ROW_S0, mux1);
 
     for (int mux2 = 0; mux2 < MUX_STATES; mux2++) {
-      setMuxPins(MUX2_A1, MUX2_A0, mux2);
-      
+      set_col(MUX_COL_S2, MUX_COL_S1, MUX_COL_S0, mux2);
       
       fdc1.triggerSingleConversion(0);
       
@@ -121,7 +193,6 @@ void loop() {
   // Serial.print("# frame_us=");
   // Serial.println(micros() - t0);
 
-  // Print all 64 values (one full frame)
   for (int i = 0; i < SCAN_PRINT; i++) {
     Serial.print(current_scan[i], 2); // 2 decimal places is faster to print than 4
     if (i < SCAN_PRINT - 1) {
